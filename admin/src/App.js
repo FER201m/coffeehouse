02bddff12b932153d2249c9d7d561e419440e6df
Bao Minh <haphanabominh9674@gmail.com>
2023-03-15 12:52:35 +0700

    config file structor

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,36 @@
+import './App.css';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { authRouter } from './router';
+import DefaultLayout from './components/layout/DefaultLayout';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        {
+          authRouter.map((router, index) => {
+            const Page = router.component;
+            let Layout = DefaultLayout;
+
+            if (router.layout) {
+              Layout = router.layout;
+            }
+
+            return <Route
+              path={router.path}
+              key={index}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            />
+
+          })
+        }
+      </Routes>
+    </Router >
+  );
 }
 
 export default App;
